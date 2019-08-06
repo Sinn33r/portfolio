4d4d40dc690568dfe40db1c77e85ba6020875431
Sinn33 <bartosz.robak@hotmail.com>
2019-08-06 15:00:02 +0200

    final

---
+++ JS/background.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particles'), {
+      dotColor: '#ede6e4',
+      lineColor: '#ede6e4',
+      minSpeedX: '0.1',
+      maxSpeedX: '0.3',
+      density: '30000',
+      
+    });
+  }, false);
